2500a8b16791a4ab58f8f97f7373a42a45f2707b
GoldSoulTheory <jtaft8514@gmail.com>
2018-06-29 14:50:27 -0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,10 @@ var knight = {hp:1350, dmg:132};
 var wizard = {hp:598, dmg:167};
 var time = 0;
 
-while (knight.hp > 0) {
-  console.log(knight.hp = knight.hp - wizard.dmg);
-  console.log(time++)
+console.log("Begin attack");
+
+while (knight.hp > 0 && wizard.hp > 0) {
+  console.log(knight.hp = knight.hp - wizard.dmg, "Knight HP");
+  console.log(wizard.hp = wizard.hp - knight.dmg, "Wizard HP");
+  console.log(time++, "seconds" );
 }
